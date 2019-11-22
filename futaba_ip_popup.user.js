3892b3bb28bc9e101fc5fa54eb610d4729387918
himuro-majika <himuro-majika@users.noreply.github.com>
2019-11-23 00:45:06 +0900

    Bump version to 1.2.5

--- futaba_ip_popup.user.js
+++ futaba_ip_popup.user.js
@@ -5,7 +5,7 @@
 // @author         himuro_majika
 // @include        http://*.2chan.net/*/res/*.htm
 // @include        https://*.2chan.net/*/res/*.htm
-// @version        1.2.4
+// @version        1.2.5
 // @grant          GM_addStyle
 // @run-at      document-idle
 // @license        MIT
